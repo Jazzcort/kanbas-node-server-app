7e0a9d091475d1f0c9f68b7347cacc209a1e9541
jazzcort <jason101011113@gmail.com>
2024-06-15 14:08:28 -0400

    Connecting with mongo

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+const CONNECTION_STRING =
+    process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 app.use(cors());
@@ -12,6 +18,7 @@ app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 
